dd0233dcd004e9f6441ecce219353a1019242064
vaishali09dak <vaishalidak91@gmail.com>
2025-07-12 16:26:42 +0530

    font change

--- client/kala-sangam/src/pages/Home.jsx
+++ client/kala-sangam/src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home({ showMap, mapRef, onStateClick }) {
   return (
     <main className="min-h-screen px-6 py-10 bg-[#fffef2] text-center scroll-smooth">
       <h1 className="text-5xl font-bold font-serif text-[#9b2226]">
-        Welcome to KalaSangam
+        Welcome to <span className="yatra-font">KalaSangam</span>
       </h1>
 
       <p className="text-lg text-[#582f0e] mt-4 max-w-2xl mx-auto">
@@ -12,7 +12,7 @@ export default function Home({ showMap, mapRef, onStateClick }) {
       </p>
 
       {/* 👇 India Map shown if `showMap` is true */}
-      {/* {showMap && (
+      {showMap && (
         <div
           id="india-map"
           ref={mapRef}
@@ -20,7 +20,7 @@ export default function Home({ showMap, mapRef, onStateClick }) {
         >
           <IndiaMap onStateClick={onStateClick} />
         </div>
-      )} */}
+      )}
     </main>
   );
 }
