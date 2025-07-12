43afe6e40fda90c7274caa857f04b030b94265bd
vaishali09dak <vaishalidak91@gmail.com>
2025-07-12 16:33:28 +0530

    remove underline

--- client/kala-sangam/src/pages/IndiaMapPage.jsx
+++ client/kala-sangam/src/pages/IndiaMapPage.jsx
@@ -4,7 +4,7 @@ import IndiaMap from '../components/IndiaMap.jsx';
 export default function IndiaMapPage({ onStateClick }) {
   return (
     <main className="min-h-screen px-6 py-10 bg-[#fffef2] text-center">
-      <h2 className="text-4xl sm:text-5xl font-bold text-[#9b2226] mb-10 tracking-wide font-serif underline decoration-[#582f0e] decoration-4 underline-offset-8">
+      <h2 className="text-4xl sm:text-5xl font-bold text-[#9b2226] mb-10 tracking-wide font-serif  decoration-[#582f0e] decoration-4 underline-offset-8">
   Explore States
 </h2>
 
