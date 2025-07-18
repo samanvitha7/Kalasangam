98f063e63c257ef44e478faadf1e7ac4546eb430
sam <bolisettysamanvitha@gmail.com>
2025-07-19 01:02:49 +0530

    fjkbghai

--- client/kala-sangam/tailwind.config.js
+++ client/kala-sangam/tailwind.config.js
@@ -6,7 +6,44 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        popFade: "popFade 1.2s ease-out both",
+        sinkFade: "sinkFade 2s ease-out forwards",
+        fadeInUp: "fadeInUp 0.6s ease-out forwards",
+        fadeIn: "fadeIn 0.3s ease-in-out forwards",
+      },
+      keyframes: {
+        popFade: {
+          "0%": {
+            opacity: "0",
+            transform: "scale(0.8) translateY(40px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "scale(1) translateY(0)",
+          },
+        },
+        sinkFade: {
+          "0%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(60vh)",
+          },
+        },
+        fadeInUp: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
   },
   plugins: [],
 }
